25312cb23488645d69d094431e470577d1e35ac3
John Farrell <johnjfarrell20@gmail.com>
2019-06-06 09:10:06 -0400

    Changed file structure a little

--- rr-godot/src/gizmos.cs
+++ rr-godot/src/gizmos.cs
@@ -3,17 +3,16 @@ using System;
 
 public class gizmos : Spatial
 {
-    private Camera mainCam;
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    Camera mainCam;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         mainCam = GetNode<Camera>("../Camera/CameraObj");
-    }
-
-    public override void _Input(InputEvent @event)
-    {
-        // GetNode("/root")._Input(@event);
     }
     public void _on_HandleX_input_event(Node camera, InputEvent @event, Vector3 click_position, Vector3 click_normal, int shape_idx)
     {
@@ -31,13 +30,10 @@ public class gizmos : Spatial
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
- public override void _Process(float delta)
- {
-    
-    var fov = mainCam.Fov;
+    public override void _Process(float delta)
+    {
+        var distance = this.GlobalTransform.origin.DistanceTo(mainCam.GlobalTransform.origin);
 
-    float distance = this.GlobalTransform.origin.DistanceTo(mainCam.GlobalTransform.origin);
-
-    this.Scale = new Vector3(distance / 4, distance / 4, distance / 4);
- }
+        this.Scale = new Vector3(distance / 4, distance / 4, distance / 4);
+    }
 }
